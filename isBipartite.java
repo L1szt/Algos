13bf4d5662674e8976be2983a23c606bcba96eb3
L1szt <77193069+L1szt@users.noreply.github.com>
2021-01-31 15:11:18 +0100

    Update isBipartite.java

--- isBipartite.java
+++ isBipartite.java
@@ -1,15 +1,15 @@
 import java.util.*;
 class isBipartite {
-    public boolean isBipartite(int[][] graph) {
-        int V = graph.length;
+    public boolean isBipartite(int g[][]) {
+        int V = g.length;
         int color[] = new int[V];  // red -> 0, blue -> 1
         boolean visited[] = new boolean[V]; 
         for (int i =0; i < V; i++) {  // graph might be disconnected
-            if (!visited[i]) if (!dfs(i, graph, visited, color, 1)) return false;
+            if (!visited[i]) if (!dfs(i, g, visited, color, 1)) return false;
         }
         return true;
     }
-    public boolean dfs(int v, int[][] g, boolean visited[], int color[], int cur) {
+    public boolean dfs(int v, int g[][], boolean visited[], int color[], int cur) {
         if (visited[v]) {
             if (color[v] != cur) return false;
             else return true;
