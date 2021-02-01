acbca208ae18f27b1f87b81e83ea5d5d53563564
L1szt <77193069+L1szt@users.noreply.github.com>
2021-02-01 18:52:09 +0100

    Update threePartition.java

--- threePartition.java
+++ threePartition.java
@@ -7,9 +7,11 @@ boolean threePartition(int arr[]) {
 		for (int i = 0; i < n+1; i++) {
 			for (int j = 0; j < sum; j++) {
 				for (int k = 0; k < sum; k++) {
-					if (i == 0 ) dp[i][j][k] = false;
-					else if (j == 0) dp[i][j][k] = true;
-					else if (k == 0) dp[i][j][k] = true;
+					if (i == 0 ) dp[i][j][k] = false;  //base case - we can't build any sum with zero elements
+					else if (j == 0) dp[i][j][k] = true;  //base case
+					else if (k == 0) dp[i][j][k] = true;  //base case
+					/* we either take the element in the first, the second or the third subset while we actually only consider the 
+					second and third subset, since if their elements sum up to A/3, this will also be the case for the remaining elements */
 					else  dp[i][j][k] = j >= arr[i-1] && k >= arr[i-1] ? dp[i - 1][j - arr[i-1]][k] || dp[i - 1][j][k - arr[i-1]] 
 						|| dp[i - 1][j][k] : j >= arr[i-1] ? dp[i - 1][j - arr[i-1]][k] || dp[i - 1][j][k]
 						 : k >= arr[i-1] ? dp[i - 1][j][k - arr[i-1]] || dp[i - 1][j][k] : dp[i - 1][j][k];
